24fc5130011f4b758dd2e19a69f8ce179f16252f
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-22 12:59:29 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(currentLocation){
+ return Math.abs(42 - currentLocation);
+}
+
+function distanceFromHqInFeet(distance){
+      return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(start,end){
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start,destination){
+    if (distanceTravelledInFeet(start, destination) < 400 ){
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000){
+        return (distanceTravelledInFeet(start, destination)-400) * 0.02;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500){
+        return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500){
+        return 'cannot travel that far';
+    }
+    
+}
